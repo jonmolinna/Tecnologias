5ca9e9582231c027644958a74adec77bd002b235
jonmolinna <jmolina2624@gmail.com>
2021-12-25 10:59:03 -0500

    CRUD

--- 4.-postgresql/1.-crud.sql
+++ 4.-postgresql/1.-crud.sql
@@ -67,6 +67,11 @@ WHERE id_tareas = 1;
 
 
 -- CREANDO BASE DE DATOS CAFE
+DROP TABLE IF EXISTS cliente_ordenes_productos;
+DROP TABLE IF EXISTS cliente_ordenes;
+DROP TABLE IF EXISTS productos;
+DROP TABLE IF EXISTS clientes;
+
 CREATE TABLE clientes (
 	cliente_id SERIAL PRIMARY KEY,
 	cliente_nombre VARCHAR(60)
@@ -75,8 +80,80 @@ CREATE TABLE clientes (
 -- campo numeric (6,2) => 1234,56
 CREATE TABLE productos (
 	producto_id SERIAL PRIMARY KEY,
-	producto_nombre VARCHAR(80),
+	producto_nombre VARCHAR(80) NOT NULL,
 	producto_precio NUMERIC(6,2) CHECK(producto_precio >= 0),
 	producto_stock SMALLINT CHECK(producto_stock >= 0) DEFAULT 0,
 	producto_tipo VARCHAR(80)
 );
+
+CREATE TABLE cliente_ordenes (
+	cliente_orden_id SERIAL PRIMARY KEY,
+	cliente_id INT REFERENCES clientes(cliente_id),
+	orden_fecha DATE DEFAULT NOW()
+);
+
+CREATE TABLE cliente_ordenes_productos (
+	cliente_orden_id INT REFERENCES cliente_ordenes(cliente_orden_id),
+	producto_id INT REFERENCES productos(producto_id),
+	cantidad SMALLINT DEFAULT 1	
+);
+
+-- Insertando registros
+INSERT INTO clientes (cliente_nombre)
+VALUES
+('Maria'),
+('Carmen'),
+('Josefa'),
+('Antonio'),
+('Jose'),
+('Manuel'),
+('David'),
+('Daniel');
+
+INSERT INTO productos (producto_nombre, producto_precio, producto_stock, producto_tipo)
+VALUES
+('Latte', 2.50, 100, 'café'),
+('Cappuccino', 1.90, 100, 'café'),
+('Dulce de leche Latte', 20.50, 100, 'café'),
+('Café Americano', 3.50, 100, 'café'),
+('Iced Café Americano', 0.50, 100, 'ice café'),
+('Café Mocha', 50, 100, 'café'),
+('Caramel Macchiato', 3.2, 100, 'café'),
+('Iced Mocha', 2.50, 100, 'ice café'),
+('Mocha Blanco', 2.50, 0, null),
+('Vanilla Latte', 2.50, 100, 'té'),
+('Flat White', 2.50, 100, 'té'),
+('Espresso', 2.50, 100, 'café'),
+('Espresso Macchiato', 0.90, 0, 'café'),
+('Espresso Con Panna', 1.50, 100, 'café'),
+('Café Cortado', 3.50, 100, 'café'),
+('Torta Cuatro Leches', 4.50, 100, 'postre'),
+('Pie de Berries', 5.50, 0, 'postre'),
+('Bagel', 2.50, 100, 'postre'),
+('Donut Rellena', 6.50, 100, 'donut'),
+('Scone de 4 quesos', 7.50, 100, 'sandwich'),
+('Muffin Zanahoria Nuez', 8.50, 100, 'muffin'),
+('Media luna', 9.50, 100, 'donut'),
+('Pan de Queso', 11.50, 100, 'sandwich'),
+('Praline Cake', 12.50, 100, 'sandwich'),
+('Lemon Cake', 22.50, 100, null),
+('Muffin con Chips', 33.50, 100, 'muffin'),
+('Muffin de Arándano', 454.50, 10, 'muffin');
+
+INSERT INTO cliente_ordenes (cliente_id)
+VALUES (3);
+
+INSERT INTO cliente_ordenes_productos (cliente_orden_id, producto_id, cantidad)
+VALUES (1, 11, 2);
+
+UPDATE productos
+SET producto_stock = producto_stock -2
+WHERE producto_id = 11;
+-- 
+
+INSERT INTO cliente_ordenes_productos (cliente_orden_id, producto_id, cantidad)
+VALUES (1, 12, 1);
+
+UPDATE productos
+SET producto_stock = producto_stock - 1
+WHERE producto_id = 12;
