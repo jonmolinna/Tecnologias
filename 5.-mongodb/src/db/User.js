ae2078496cddcb14c6169aa6523318bc25218625
jonmolinna <jmolina2624@gmail.com>
2022-01-09 17:28:39 -0500

    Mongoose

--- 5.-mongodb/src/db/User.js
+++ 5.-mongodb/src/db/User.js
@@ -5,6 +5,18 @@ const userSchema = mongoose.Schema({
     username: String,
     edad: Number,
     estado: Boolean,
+    address: {
+        postal: Number,
+        pais: String,
+    },
+    cursos: Array,
+    comments: [
+        {
+            body: String,
+            like: Boolean,
+            tags: Array,
+        }
+    ]
 });
 
 export default mongoose.model('user', userSchema);
--- 5.-mongodb/src/rutas/index.js
+++ 5.-mongodb/src/rutas/index.js
@@ -2,7 +2,13 @@ import express from "express";
 const router = express.Router();
 
 import { insertManyUser, createUser } from '../controllers/insert.js';
-import { query1, query2, query3, query4, query5, query6, query7, query8, query9, query10, query11 } from '../controllers/consultas.js';
+import {
+  query1, query2, query3, query4, query5,
+  query6, query7, query8, query9, query10, query11 } from '../controllers/consultas.js';
+import { queryCount, queryLimit, querySkip, querySort } from '../controllers/cursor.js';
+import { proyeccion } from '../controllers/proyecciones.js';
+import { updateUserSave, updateUser, findByIdAndUpdateUser } from '../controllers/update.js';
+import { deleteOneUser, findByIdAndDeleteUser } from '../controllers/delete.js';
 
 // INSERT
 router.post('/insert', insertManyUser);
@@ -20,5 +26,23 @@ router.get('/query8', query8);
 router.get('/query9', query9);
 router.get('/query10', query10);
 router.get('/query11', query11);
+
+// QUERYCURSOR
+router.get('/queryCount', queryCount);
+router.get('/queryLimit', queryLimit);
+router.get('/querySkip', querySkip);
+router.get('/querySort', querySort);
+
+// PROYECCIONES
+router.get('/proyeccion', proyeccion);
+
+// UPDATE
+router.put('/updateUser/:id', updateUserSave);
+router.put('/updateUser/:id', updateUser);
+router.put('/findByIdAndUpdateUser/:id', findByIdAndUpdateUser);
+
+// DELETE
+router.delete('/deleteOneUser/:id', deleteOneUser);
+router.delete('/findByIdAndDeleteUser/:id', findByIdAndDeleteUser);
 
 export default router;
