405d82ce1d9b746dbb7df51276e96ed457d551ba
jonmolinna <jmolina2624@gmail.com>
2022-01-09 17:28:02 -0500

    Insert

--- 5.-mongodb/src/controllers/insert.js
+++ 5.-mongodb/src/controllers/insert.js
@@ -18,10 +18,11 @@ export const insertManyUser = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-    const { name, username, edad, estado } = req.body;
+    // const { name, username, edad, estado } = req.body;
 
     try {
-        const user = await User.create({ name, username, edad, estado });
+        // const user = await User.create({ name, username, edad, estado });
+        const user = await User.create(req.body);
         
         return res.status(200).json({
             message: 'User Create',
